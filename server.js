7e7d094c16daac868ac337b963b2729a1c04df83
Malisha Lakshan <malishalakshandev@gmail.com>
2020-04-20 14:27:50 +0530

    Add Project page

--- server.js
+++ server.js
@@ -61,6 +61,12 @@ app.get('/about',(req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
+
 /* bad page */
 app.get('/bad', (req, res) => {
   res.send({
